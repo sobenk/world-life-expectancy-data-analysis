190df67e4441c0a537953be2ec8dab4775ede63d
sobenk <167455531+sobenk@users.noreply.github.com>
2024-04-29 00:13:23 +0200

    Update World Life Expectancy Data Analysis.sql
    fix

--- World Life Expectancy Data Analysis.sql
+++ World Life Expectancy Data Analysis.sql
@@ -1,12 +1,5 @@
-# Présentation de l'étude de cas : Analyse exploratoire des données de l'espérance de vie mondiale
 
-Dans cette partie de l'étude de cas, nous avons effectué une analyse exploratoire des données sur l'espérerance de vie mondiale.
-L'objectif est de découvrir des tendances et des insights pertinentes dans les données.
-
-## Commentaires sur le code et explications
-
-1. Nous avons commencé par examiner l'espérance de vie par pays et calculer l'augmentation de l'esperance de vie sur 15 ans.
-```sql
+-- Nous avons commencé par examiner l'espérance de vie par pays et calculer l'augmentation de l'esperance de vie sur 15 ans.
 SELECT Country,
 MIN(`Life expectancy`), 
 MAX(`Life expectancy`),
@@ -18,36 +11,30 @@ AND MAX(`Life expectancy`) <> 0
 ORDER BY Country DESC;
 ```
 
-2. Ensuite, nous avons calculé l'espérance de vie moyenne par année pour déduire une tendance générale.
-```sql
+--  Ensuite, nous avons calculé l'espérance de vie moyenne par année pour déduire une tendance générale.
 SELECT Year, ROUND(AVG(`Life expectancy`),2)
 FROM world_life_expectancy
 WHERE `Life expectancy` <> 0
 AND `Life expectancy` <> 0
 GROUP BY YEAR
 ORDER BY YEAR;
-```
 
-3. Nous avons également examiné la tendance du PIB (variable _GDP_) par pays et son lien avec l'espérance de vie.
-```sql
+-- Nous avons également examiné la tendance du PIB (variable _GDP_) par pays et son lien avec l'espérance de vie.
 SELECT Country, ROUND(AVG(`Life expectancy`),1) AS Life_Exp, ROUND(AVG(GDP),1) AS GDP
 FROM world_life_expectancy
 GROUP BY Country
 HAVING Life_Exp >0
 ORDER BY GDP ASC;
-```
-Nous avons constaté qu'il existe uen corrélation entre le PIB et l'espérance de vie : une espérance de vie plus basse est généralement
-associée à un PIB plus bas.
 
-4. Enfin, nous avons utilisé une instruction __CASE__ pour catérogiser les pays en fonction de leur PIB.
-```sql
+-- Nous avons constaté qu'il existe uen corrélation entre le PIB et l'espérance de vie : une espérance de vie plus basse est généralement associée à un PIB plus bas.
+
+-- Enfin, nous avons utilisé une instruction __CASE__ pour catérogiser les pays en fonction de leur PIB.
 SELECT
 SUM(CASE WHEN GDP >= 1500 THEN 1 ELSE 0 END) High_GDP_Count,
 AVG(CASE WHEN GDP >= 1500 THEN `Life expectancy` ELSE NULL END) High_GDP_Life_Expectancy,
 SUM(CASE WHEN GDP <= 1500 THEN 1 ELSE 0 END) Low_GDP_Count,
 AVG(CASE WHEN GDP <= 1500 THEN `Life expectancy` ELSE NULL END) Low_GDP_Life_Expectancy
-FROM world_life_expectancy
-;
-```
-Nous avons calculé l'espérance de vie moyenne pour les pays avec un PIB élevé et un PIB faible.
-On constate que l'espérance de vie est généralement plus élevé dans les pays avec un PIB elevé.
+FROM world_life_expectancy;
+
+-- Nous avons calculé l'espérance de vie moyenne pour les pays avec un PIB élevé et un PIB faible.
+-- On constate que l'espérance de vie est généralement plus élevé dans les pays avec un PIB elevé.
